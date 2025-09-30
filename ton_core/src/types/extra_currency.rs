f0da18a430c8b76a3d005c93b0ef885bfc56b57a
nikita <n.bokarev@ston.fi>
2025-09-30 16:22:45 +0300

    review refactor

--- ton_core/src/types/extra_currency.rs
+++ ton_core/src/types/extra_currency.rs
@@ -1,15 +1,12 @@
-use std::num::ParseIntError;
-use thiserror::Error;
+use crate::{cell::TonHash, errors::TonCoreError, types::TonAddress};
 
-use crate::{cell::TonHash, types::TonAddress};
-
-const EXTRA_CURRENCY_BASE_HASH: [u8; TonHash::BYTES_LEN] = {
+const EXTRA_CURRENCY_BASE_HASH: TonHash = {
     let mut prefix = [0u8; TonHash::BYTES_LEN];
 
     // First bit marks that this is extra currency
     prefix[0] = 1 << 7;
 
-    prefix
+    TonHash::from_slice_sized(&prefix)
 };
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
@@ -18,34 +15,33 @@ pub struct TonExtraCurrencyId(u32);
 impl TonExtraCurrencyId {
     pub fn new(id: u32) -> TonExtraCurrencyId { TonExtraCurrencyId(id) }
 
-    pub fn to_address(id: TonExtraCurrencyId) -> TonAddress {
+    pub fn to_address(&self) -> TonAddress {
         let mut hash_part = EXTRA_CURRENCY_BASE_HASH;
 
-        let id_be_bytes = u32::from(id).to_be_bytes();
+        let id_be_bytes = self.to_be_bytes();
 
-        let ([.., b3_mut, b2_mut, b1_mut, b0_mut], [b3, b2, b1, b0]) = (&mut hash_part, id_be_bytes);
+        let ([.., b3_mut, b2_mut, b1_mut, b0_mut], [b3, b2, b1, b0]) = (hash_part.as_slice_sized_mut(), id_be_bytes);
         *b0_mut = b0;
         *b1_mut = b1;
         *b2_mut = b2;
         *b3_mut = b3;
 
-        TonAddress::new(0, hash_part.into())
+        TonAddress::new(0, hash_part)
     }
 
-    pub fn from_address(address: &TonAddress) -> Option<TonExtraCurrencyId> {
-        if EXTRA_CURRENCY_BASE_HASH[0..(TonHash::BYTES_LEN - 4)] != address.hash.as_slice()[0..(TonHash::BYTES_LEN - 4)]
+    pub fn from_address(address: &TonAddress) -> Result<TonExtraCurrencyId, TonCoreError> {
+        if EXTRA_CURRENCY_BASE_HASH.as_slice_sized()[0..(TonHash::BYTES_LEN - 4)]
+            != address.hash.as_slice()[0..(TonHash::BYTES_LEN - 4)]
         {
-            return None;
+            return Err(TonCoreError::data("TonExtraCurrencyId", "Address hash mismatch"));
         };
 
         let id_bytes = match address.hash.as_slice() {
             [.., b3, b2, b1, b0] => [*b3, *b2, *b1, *b0],
-            _ => return None,
+            _ => return Err(TonCoreError::data("TonExtraCurrencyId", "Not enough bytes in address hash")),
         };
 
-        let id = u32::from_be_bytes(id_bytes).into();
-
-        Some(id)
+        Ok(Self(u32::from_be_bytes(id_bytes)))
     }
 }
 
@@ -54,7 +50,8 @@ mod traits_impl {
     use std::ops::Deref;
     use std::str::FromStr;
 
-    use crate::types::{TonExtraCurrencyId, TonExtraCurrencyIdParseError};
+    use crate::errors::TonCoreError;
+    use crate::types::TonExtraCurrencyId;
 
     impl Display for TonExtraCurrencyId {
         fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result { self.0.fmt(formatter) }
@@ -70,13 +67,7 @@ mod traits_impl {
         fn from(value: TonExtraCurrencyId) -> Self { value.0 }
     }
     impl FromStr for TonExtraCurrencyId {
-        type Err = TonExtraCurrencyIdParseError;
+        type Err = TonCoreError;
         fn from_str(string: &str) -> Result<Self, Self::Err> { Ok(u32::from_str(string)?.into()) }
     }
-}
-
-#[derive(Debug, Error)]
-pub enum TonExtraCurrencyIdParseError {
-    #[error("not an unsigned 32-bit number")]
-    ParseIntError(#[from] ParseIntError),
 }
