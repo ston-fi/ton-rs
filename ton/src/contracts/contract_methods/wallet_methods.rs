b9d91153d8d41e3a621d7eaa45496a8af27124a4
Valerii Kokorev <v.kokorev@ston.fi>
2025-12-17 16:04:31 +0400

    wip

--- ton/src/contracts/contract_methods/wallet_methods.rs
+++ ton/src/contracts/contract_methods/wallet_methods.rs
@@ -7,12 +7,10 @@ use ton_core::cell::TonHash;
 #[async_trait]
 pub trait TonWalletMethods: TonContract {
     async fn seqno(&self) -> Result<u32, TonError> {
-        let wallet_seqno: u32 = self.emulate_get_method("seqno", &TVMStack::EMPTY, None).await?;
-        Ok(wallet_seqno)
+        self.emulate_get_method("seqno", &TVMStack::EMPTY, None).await
     }
 
     async fn get_public_key(&self) -> Result<TonHash, TonError> {
-        let wallet_pk: TonHash = self.emulate_get_method("get_public_key", &TVMStack::EMPTY, None).await?;
-        Ok(wallet_pk)
+       self.emulate_get_method("get_public_key", &TVMStack::EMPTY, None).await
     }
 }
